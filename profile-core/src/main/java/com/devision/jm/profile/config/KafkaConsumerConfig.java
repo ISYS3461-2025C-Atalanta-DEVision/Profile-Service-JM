3e9bf85dc9704370ab42ddaf012a98a042c804c0
thaobwi <129365430+nancybwi@users.noreply.github.com>
2025-12-28 00:46:55 +0700

    Update KafkaConsumerConfig.java

--- profile-core/src/main/java/com/devision/jm/profile/config/KafkaConsumerConfig.java
+++ profile-core/src/main/java/com/devision/jm/profile/config/KafkaConsumerConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -22,7 +23,7 @@ public class KafkaConsumerConfig {
 
     private final KafkaDiscoveryService kafkaDiscoveryService;
 
-    @Bean
+@Bean
     public ConsumerFactory<String, CompanyNameRequestEvent> companyNameRequestConsumerFactory() {
         Map<String, Object> props = new HashMap<>();
         String bootstrapServers = kafkaDiscoveryService.getKafkaBootstrapServers();
@@ -30,17 +31,16 @@ public class KafkaConsumerConfig {
 
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                org.springframework.kafka.support.serializer.JsonDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 
-        // Option A (recommended for now): Trust all
-        props.put(org.springframework.kafka.support.serializer.JsonDeserializer.TRUSTED_PACKAGES, "*");
+        // Tell JsonDeserializer to use our local class, and ignore type headers
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE,
+                "com.devision.jm.profile.api.external.dto.CompanyNameEvent.CompanyNameRequestEvent");
+        props.put(JsonDeserializer.USE_TYPE_INFO_HEADERS, false);
 
-        // Optionally keep VALUE_DEFAULT_TYPE, but it is not required if we trust all
-        // and the producer sends __TypeId__ header.
-        // You can either remove it, or set it to your local class:
-        // props.put(org.springframework.kafka.support.serializer.JsonDeserializer.VALUE_DEFAULT_TYPE,
-        // "com.devision.jm.profile.api.external.dto.CompanyNameEvent.CompanyNameRequestEvent");
+        // Trusted packages can be just your own package now
+        props.put(JsonDeserializer.TRUSTED_PACKAGES,
+                "com.devision.jm.profile.api.external.dto.CompanyNameEvent");
 
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "profile-service-group");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
